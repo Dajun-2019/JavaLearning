2dd918fd399994a63b1f0bf869b79fa6cea8cfae
Dajunnnnnn <1064049895@qq.com>
2021-10-12 23:47:39 +0800

    Springboot in action

---
+++ readingList/src/test/java/com/example/readingList/MockMvcWebTests.java
@@ -0,0 +1,54 @@
+package com.example.readingList;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Matchers;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = ReadingListApplication.class)
+@WebAppConfiguration
+public class MockMvcWebTests {
+
+    @Autowired
+    private WebApplicationContext webContext;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setupMockMvc(){
+        mockMvc = MockMvcBuilders
+                .webAppContextSetup(webContext)
+                .apply(springSecurity())
+                .build();
+    }
+    @Test
+    public void homePage() throws Exception{
+        mockMvc.perform(MockMvcRequestBuilders.get("/readingList"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.view().name("readingList"))
+                .andExpect(MockMvcResultMatchers.model().attributeExists("books"))
+                .andExpect(MockMvcResultMatchers.model().attribute("books",
+                        Matchers.isNull()));
+    }
+    @Test
+    @WithMockUser(username = "")
+    public void homePage_unauthenticatedUser() throws Exception{
+        mockMvc.perform(MockMvcRequestBuilders.get("/"))
+                .andExpect(MockMvcResultMatchers.status().is3xxRedirection())
+                .andExpect(MockMvcResultMatchers.header().string("Location","http://localhost/login"));
+    }
+}
+
